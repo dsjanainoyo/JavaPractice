46ab94e8080dcab38b7f1fa7bece6ef9b8e36b69
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-01-30 23:11:13 +0900

    practice comment

---
+++ src/JSample6_1.java
@@ -0,0 +1,22 @@
+/**
+ * JSample6_1
+ * 
+ *2023.1.30
+ */
+public class JSample6_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int sum = 0;
+		
+		//10回繰り返す
+		for (int i =1; i <= 10; i++) {
+			sum = sum + i; //数値を順に加算する
+		}
+		
+		/*
+		 * 集計した結果を画面に出力する
+		 */
+		System.out.println(sum);
+	}
+}
