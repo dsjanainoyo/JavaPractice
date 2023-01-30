43f2e10e1d0a7c85c6586ee04964db31c1f0c716
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-01-30 23:16:31 +0900

    practice 処理順

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World");
+		System.out.println("How are you?");
+		System.out.println("Bye");
 	}
 
 }
