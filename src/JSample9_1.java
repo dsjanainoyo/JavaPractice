515658c5681b9cb19d25941b1e5067ad873c0560
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-01-31 00:55:19 +0900

    practice import

---
+++ src/JSample9_1.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class JSample9_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		LocalDate Id = LocalDate.now();
+		System.out.println(Id);
+	}
+
+}
