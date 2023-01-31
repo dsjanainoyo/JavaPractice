6b5dffcb87a8a592cf6c4b1b001736957bd4c434
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-02-01 01:00:24 +0900

    practice literal

---
+++ src/JSample3_1.java
@@ -0,0 +1,10 @@
+
+public class JSample3_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World");
+		System.out.println("こんにちは");
+	}
+
+}
---
+++ src/JSample4_1.java
@@ -0,0 +1,11 @@
+
+public class JSample4_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println('\'');
+		System.out.println("私の名前は\"鈴木\"です");
+		System.out.println("この魚の値段は¥250です");
+	}
+
+}
