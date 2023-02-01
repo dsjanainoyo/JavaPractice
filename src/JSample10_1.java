f139ed094b6d5fd804bdb2f1b2bcb8a4da25d8d0
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-02-02 01:50:39 +0900

    practice literal

---
+++ src/JSample10_1.java
@@ -0,0 +1,10 @@
+
+public class JSample10_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(10 > 7);
+		System.out.println(5 == 4);
+	}
+
+}
---
+++ src/JSample8_1.java
@@ -0,0 +1,9 @@
+
+public class JSample8_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(2200000000L);
+	}
+
+}
